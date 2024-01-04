1bca315d1127839ef01a817826ae251c3b77e47d
Karthick-Scode <karthiscode@gmail.com>
2024-01-04 16:26:25 +0530

    first commit

---
+++ app/src/main/java/com/example/youtubeplayer/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.youtubeplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.os.Bundle;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "3S66-qL4qkQ";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+}
